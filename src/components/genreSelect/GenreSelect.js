1deb6087f3acf2db2622e66a920cd2b54431a906
Dmytro Shlyhin <dima.shlugin@gmail.com>
2023-09-13 16:55:30 +0300

    update GenreSelect to 3 props

--- src/components/genreSelect/GenreSelect.js
+++ src/components/genreSelect/GenreSelect.js
@@ -1,30 +1,13 @@
 import React, { useState } from 'react';
 import './genreSelect.css';
 
-function GenreSelect({ genres, onSelect }) {
+function GenreSelect({ genres, onSelect, selectedOptions  }) {
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedOptions, setSelectedOptions] = useState([]);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
-  const handleOptionClick = (genre) => {
-    if (selectedOptions.includes(genre)) {
-      setSelectedOptions((prevSelected) => {
-        const newSelected = prevSelected.filter((g) => g !== genre);
-        onSelect(newSelected);
-        return newSelected;
-      });
-    } else {
-      setSelectedOptions((prevSelected) => {
-        const newSelected = [...prevSelected, genre];
-        onSelect(newSelected);
-        return newSelected;
-      });
-    }
-  };
-  
 
   return (
     <div className="genre-select" >
@@ -33,7 +16,7 @@ function GenreSelect({ genres, onSelect }) {
       {isOpen && (
         <div className="genre-selects-container">
           {genres.map((genre) => (
-            <div className="genre-select-container" key={genre}  >
+            <div className="genre-select-container" key={genre} >
                 <input 
                     id={genre}
                     className="genre-select-selects" 
@@ -41,7 +24,7 @@ function GenreSelect({ genres, onSelect }) {
                     checked={selectedOptions.includes(genre)} 
                     onChange={() =>  {}}
                 />
-                <label onClick={()=> handleOptionClick(genre)} htmlFor={genre} className="genre-select-selects-label">
+                <label onClick={()=> onSelect(genre)} htmlFor={genre} className="genre-select-selects-label">
                     {genre}
                 </label> 
             </div>
